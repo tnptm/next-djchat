f8f4c544a6c07a54e3f43ad8879a68d6d89ecb7d
Toni Patama <tonipat047@gmail.com>
2025-11-12 15:03:27 +0200

    Refactor: Update STATIC_URL and MEDIA_URL settings to use absolute paths and environment variables for better configuration

--- djchat/djchat/settings.py
+++ djchat/djchat/settings.py
@@ -152,13 +152,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = "/static/"
+STATIC_ROOT = os.getenv("DJANGO_STATIC_ROOT", BASE_DIR / "staticfiles")
 
 # Media files (User uploaded files)
-MEDIA_URL = "media/"
-MEDIA_ROOT = BASE_DIR / "media"
-
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.getenv("DJANGO_MEDIA_ROOT", BASE_DIR / "media")
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
